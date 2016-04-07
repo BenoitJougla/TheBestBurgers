e5c9ff31fbf0b222cee1282e785197d32ba0aea9
unknown <jougla.benoit@hotmail.fr>
2016-04-07 21:59:38 +0200

    Remove cache for IE

--- src/main/java/presentation/get/IngredientServlet.java
+++ src/main/java/presentation/get/IngredientServlet.java
@@ -21,6 +21,13 @@ public class IngredientServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final List<Ingredient> ingredientsList = DAOManager.getInstance().getAllIngredients();
         final String json = new Gson().toJson(ingredientsList);
+
+        // Set standard HTTP/1.1 no-cache headers.
+        resp.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+
+        // Set standard HTTP/1.0 no-cache header.
+        resp.setHeader("Pragma", "no-cache");
+
         resp.getOutputStream().write(json.getBytes());
     }
 
